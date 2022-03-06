77acc19168aa54d41f2056c2f47ac6cf3db28360
audgks5551 <audgks5551@daum.net>
2022-03-06 14:17:54 +0900

    테스트 기능 추가 및 리팩토링

--- src/main/java/springbook/test/UserDaoTest.java
+++ src/main/java/springbook/test/UserDaoTest.java
@@ -1,8 +1,10 @@
 package springbook.test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -13,22 +15,58 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @Before
+    public void setUp() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.userDao = context.getBean("userDao", UserDao.class);
+        user1 = new User("1", "이명한", "123");
+        user2 = new User("2", "이명한", "123");
+        user3 = new User("3", "이명한", "123");
+    }
+
     @Test
     public void addAndGet() throws SQLException {
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
 
-        User user = new User();
-        user.setId("hyumee1");
-        user.setName("김유림");
-        user.setPassword("springno1");
+        User findUser1 = userDao.get(user1.getId());
+        assertThat(findUser1.getName(), is(findUser1.getName()));
+        assertThat(findUser1.getPassword(), is(findUser1.getPassword()));
 
-        userDao.add(user);
+        User findUser2 = userDao.get(user2.getId());
+        assertThat(findUser2.getName(), is(findUser2.getName()));
+        assertThat(findUser2.getPassword(), is(findUser2.getPassword()));
+    }
 
-        User findUser = userDao.get(user.getId());
+    @Test
+    public void count() throws SQLException {
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
 
-        assertThat(findUser.getName(), is(user.getName()));
-        assertThat(findUser.getPassword(), is(user.getPassword()));
+        userDao.add(user1);
+        assertThat(userDao.getCount(), is(1));
+
+        userDao.add(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        userDao.add(user3);
+        assertThat(userDao.getCount(), is(3));
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void getUserFailure() throws SQLException {
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.get("unknown_id");
     }
 }
